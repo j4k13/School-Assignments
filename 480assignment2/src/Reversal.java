eb2136ffce2378c7db4b932888154324f17cce7a
Jackie <namlesscrusader@gmail.com>
2015-10-17 19:59:56 -0400

    Added test

--- 480assignment2/src/Reversal.java
+++ 480assignment2/src/Reversal.java
@@ -1,4 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Reversal {
-
+	public static void reverseFile(File input , File output)
+	{
+		
+	}
 }
--- 480assignment2/src/ReversalTest.java
+++ 480assignment2/src/ReversalTest.java
@@ -1,12 +1,66 @@
 import static org.junit.Assert.*;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import org.junit.Test;
 
 public class ReversalTest {
 
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testreverseFile()
+	{
+	//File input;
+	//File output;
+	//case that check for exception with no file
+	
+	//case that checks for moving punctuation
+	try
+	{
+		File input = new File("file");
+		File output = new File("file");
+		File expected = new File("file");
+		PrintWriter outin = new PrintWriter(input);
+		PrintWriter outout = new PrintWriter(expected);
+		Scanner in = new Scanner(input);
+		outin.println("it. it,");
+		outout.println("it, it.");
+		Reversal.reverseFile(input,output);
+		assertEquals("",output,expected);
+		outin.close();
+		outout.close();
+		in.close();
 	}
-
+	catch(FileNotFoundException e)
+	{
+		
+	}
+	//case with several lines to check that it works with multiple lines
+	try
+	{
+		File input = new File("file");;
+		File output = new File("file");
+		File expected = new File("file");
+		PrintWriter outin = new PrintWriter(input);
+		PrintWriter outout = new PrintWriter(expected);
+		Scanner in = new Scanner(input);
+		outin.println("it. it,");
+		outin.println("since then");
+		outout.println("then since");
+		outout.println("it, it.");
+		Reversal.reverseFile(input,output);
+		assertEquals("",output,expected);
+		outin.close();
+		outout.close();
+		in.close();
+	}
+	catch(FileNotFoundException e)
+	{
+		
+	}
+	}
 }
