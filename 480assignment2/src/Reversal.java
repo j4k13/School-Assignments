4920a3700c0dd74d77957a31862e2133fedb1e15
Jackie <namlesscrusader@gmail.com>
2015-10-18 17:06:02 -0400

    added the bulk of the code

--- 480assignment2/src/Reversal.java
+++ 480assignment2/src/Reversal.java
@@ -1,12 +1,51 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ *
+ * @author Jacqueline Anderson
+ */
 public class Reversal {
-	public static void reverseFile(File input , File output)
-	{
-		
-	}
+	
+	
+    public static void reverseFile(File input,File output)throws FileNotFoundException
+    {
+    	//line currently being reversed
+    	String currentchunk;
+    	String linebuilding = "";
+    	ArrayList <String> lineholder = new ArrayList <String>();
+    	try 
+    	{
+			Scanner in = new Scanner(input);
+			PrintWriter out = new PrintWriter(output);
+			while(in.hasNextLine())
+			{
+				Scanner in2 = new Scanner(in.nextLine());
+				linebuilding = "";
+				while(in2.hasNext())
+				{
+					currentchunk = in2.next();
+					linebuilding = currentchunk + " " + linebuilding;
+				}
+				lineholder.add(linebuilding);
+				in2.close();
+			}
+			for(int i = 0 ; i < lineholder.size(); i++)
+			{
+				out.println(lineholder.get(lineholder.size() - i));
+			}
+			in.close();
+			out.close();
+		} 
+    	catch (FileNotFoundException e) 
+    	{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
 }
